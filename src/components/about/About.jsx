2aa22c7ea9296f89b15e9ab6e665ef0dd18da805
Bradley Shaffer <magezone@Magnezone.local>
2025-02-03 07:12:24 -0500

    moving bg

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './about.css'
 import headShot from '../../assets/headshot.jpeg'
-import myImage from '../../assets/remy_stand.gif'
+import idleRemy from '../../assets/remy_stand.gif'
 import wFwdRemy from '../../assets/remy_w_fwd.gif'
 import wBackRemy from '../../assets/remy_w_back.gif'
 import CTA from './CTA'
@@ -10,21 +10,40 @@ const About = () => {
   const [currentPicture, setPicture] = useState(wFwdRemy);
   const [spriteToggle, setSpriteToggle] = useState("");
 
+  const [position, setPosition] = useState(0);
+  const [direction, setDirection] = useState(-1); // -1 for left, 1 for right
+
+
+  useEffect(() => {
+    if (direction !== 0) {
+      const interval = setInterval(() => {
+        setPosition((prev) => prev + direction * 5);
+      }, 30);
+
+      return () => clearInterval(interval);
+    }
+  }, [direction]);
+
+
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.key === 'ArrowLeft') {
         setPicture(wBackRemy);
+        setDirection(1);
       }
       if (event.key === 'ArrowRight') {
         setPicture(wFwdRemy);
+        setDirection(-1);
       }
     }
     const handleKeyUp = (event) => {
       if (event.key === 'ArrowLeft') {
-        setPicture(myImage);
+        setPicture(idleRemy);
+        setDirection(0);
       }
       if (event.key === 'ArrowRight') {
-        setPicture(myImage);
+        setPicture(idleRemy);
+        setDirection(0);
       }
     }
     window.addEventListener('keydown', handleKeyDown);
@@ -43,12 +62,13 @@ const About = () => {
 
       <div className="container about__container">
 
-          <div className="about__me">
+          <div className="about__me" style={{ backgroundPositionX: position }}>
             <div className="about__me-image">
+
                 <img src={currentPicture} alt="Me" className={spriteToggle} 
                 onClick={()=>{
                   if(currentPicture === headShot){
-                    setPicture(myImage);
+                    setPicture(idleRemy);
                     setSpriteToggle("sprite");
                   } else {
                     setPicture(headShot);
@@ -64,6 +84,7 @@ const About = () => {
                     I've spent the last 10 years working in Adtech, at companies like Criteo, Triplelift, and Experian. <br></br> <br></br>
                     Throughout this time, I've held various roles across the Engineer-Commercial spectrum; including development focused roles (Software Engineer) as well as client facing engineer roles (Solutions Engineer, TAM, etc)
             </p>
+            
             <CTA/>
           </div>
       </div>
